3d2b514dbe2451ef9bcb20f25f87bc1c0df32477
Sung Ho Kim <likeleon@gmail.com>
2016-08-04 22:35:10 +0900

    북 선택시 첫 이미지 보여주기

--- ZangSiSee/Pages/ComicBooksPage.xaml.cs
+++ ZangSiSee/Pages/ComicBooksPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ZangSiSee.Models;
+﻿using Xamarin.Forms;
+using ZangSiSee.Models;
 using ZangSiSee.ViewModels;
 
 namespace ZangSiSee.Pages
@@ -18,6 +19,31 @@ namespace ZangSiSee.Pages
             Title = ViewModel.Comic.Title;
 
             await ViewModel.GetBooks().ConfigureAwait(false);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            list.ItemSelected += OnListItemSelected;
+        }
+
+        protected override void OnDisappearing()
+        {
+            list.ItemSelected -= OnListItemSelected;
+
+            base.OnDisappearing();
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (list.SelectedItem == null)
+                return;
+
+            var vm = list.SelectedItem as BookViewModel;
+            list.SelectedItem = null;
+
+            await Navigation.PushAsync(new BookImagesPage(vm.Book));
         }
     }
 
